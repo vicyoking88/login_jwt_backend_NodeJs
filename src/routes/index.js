37544f309f3e0f18083bbca1f0f508163fe6a1a9
Victor Medina <vamedinac@icloud.com>
2021-12-12 11:22:59 -0500

    cargamos_devolvemos_token

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
 const {Router}=require('express');
 const router=Router();
-
+//cargamos el modelo user
 const user = require('../models/User');
+
+//cargamos el modulo para el token
+const jwt=require('jsonwebtoken');
 
 router.get('/',(req, res)=>res.send('Hello World'))
 
@@ -21,8 +24,11 @@ router.post('/signup', async (req, res)=>{
     //guardamos en mongodb
     /**await y async para seguir trabajando mientras se ejecuta el proceso asincrono en este caso el .save() */
     await newUser.save();
-
-    res.send('Testing Signup')
+    //usamos el metodo sign del modulo
+    //guardamos en una constante el token
+    const token=jwt.sign({_id:newUser._id}, 'secretKey')
+    //devuelvo el token al cliente
+    res.status(200).json({token:token})
 })
 
 module.exports=router;
