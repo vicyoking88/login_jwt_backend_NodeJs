568b2247631865cb68f975359c0660a59cc8a85c
Victor Medina <vamedinac@icloud.com>
2021-12-12 12:16:16 -0500

    RUTA_data_public

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,6 +8,7 @@ const jwt=require('jsonwebtoken');
 
 router.get('/',(req, res)=>res.send('Hello World'))
 
+//ruta creacion usuario y devolucion token
 router.post('/signup', async (req, res)=>{
     //vemos el cuerpo: body de la peticion: req quest
     console.log(req.body)
@@ -29,6 +30,46 @@ router.post('/signup', async (req, res)=>{
     const token=jwt.sign({_id:newUser._id}, 'secretKey')
     //devuelvo el token al cliente
     res.status(200).json({token:token})
+})
+
+//ruta iniciar sesion usuario ya creado
+router.post('/signin', async (req, res)=>{
+    //sacamos el email y el password de la peticion
+    const{email, password}=req.body;
+    //buscamos un elemento en el modelo
+    //si lo encuentra lo guarda en una constante llamada user
+    const User= await user.findOne({email:email})
+
+    if(!User) return res.status(401).send("the email doesn't exists");
+    if(User.password !== password) return res.status(401).send("Wrong Password");
+
+    const token=jwt.sign({_id: user._id}, 'secretKey');
+
+    return res.status(200).json({token});
+})
+
+/**RUTA EJEMPLO PARA SOLICITAR INFORMACION DATOS PUBLICOS SIN AUTENTICACION*/
+router.get('/tasks', (req, res)=>{
+    res.json([
+        {
+            _id:1,
+            name: 'task one',
+            description: 'test token',
+            date:'2021-12-12T16:03:21.548Z'
+        },
+        {
+            _id:2,
+            name: 'task two',
+            description: 'test token',
+            date:'2021-12-12T16:03:21.548Z'
+        },
+        {
+            _id:3,
+            name: 'task three',
+            description: 'test token',
+            date:'2021-12-12T16:03:21.548Z'
+        }
+    ])
 })
 
 module.exports=router;
