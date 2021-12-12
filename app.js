2616603873fd9aff5e962cb62a8a788f9363f441
Victor Medina <vamedinac@icloud.com>
2021-12-12 10:52:56 -0500

    nodemon_post_json_route

--- app.js
+++ app.js
@@ -2,10 +2,14 @@
 const express = require('express')
 //ejecutamos modulo express que me devuelve un objeto que lo guardo en app
 const app= express();
+//usar de express el metodo json para que interprete todos los json
+app.use(express.json());
 
+//conexion a la base de datos
 require('./src/database');
 
-app.use(require('./src/routes/index'))
+//todas las rutas se les antepone /api
+app.use('/api',require('./src/routes/index'))
 
 app.listen(3000);
 console.log('server on port', 3000);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,14 @@
 const {Router}=require('express');
 const router=Router();
 
+const user = require('../models/User');
 
-router.get('/',(req, res)=>
-res.send('Hello World'))
+router.get('/',(req, res)=>res.send('Hello World'))
 
-
+router.post('/signup', (req, res)=>{
+    //vemos el cuerpo: body de la peticion: req quest
+    console.log(req.body)
+    res.send('Testing Signup')
+})
 
 module.exports=router;
